288b91b61275ddea4e556a731d0e04719971793a
Andrei2348 <makarevichandco@gmail.com>
2024-10-10 13:14:25 +0300

    Add new components

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang='ts' setup>
+console.log('loaded')
+</script>
+
+<template>
+<section class='section'>
+<LayoutSidebar />
+<div>fsdfdfdsfdsfsdsfsdfdsfd</div>
+<slot />
+</section>
+</template>
+
+<style scoped lang='scss'>
+.section{
+	display: grid;
+	grid-template-columns: 1fr 6fr;
+	min-height: 100vh;
+}
+</style>
