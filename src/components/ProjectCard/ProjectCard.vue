c44cc5c917799de1b9efeebebb303c780f11c585
Andrei2348 <makarevichandco@gmail.com>
2024-10-24 16:47:43 +0300

    add more styles for project

--- src/components/ProjectCard/ProjectCard.vue
+++ src/components/ProjectCard/ProjectCard.vue
@@ -2,7 +2,9 @@
 	<div class='card__wrapper'>
 		<h3 class='card__title'>{{project.name}}</h3>
 		<p class='card__text'>Участники: {{project.users.length}} человек.</p>
-		<Router-link to='/' class='card__button'>Перейти к проекту</Router-link>
+		<div class="card__buttons-wrapper">
+			<Router-link to='/' class='card__button'><SvgIcon icon='chevron-right'/></Router-link>
+		</div>
 	</div>
 </template>
 
