bfe6f882a37fd0143fc67658c39d3d373ad77c64
Andrei2348 <makarevichandco@gmail.com>
2024-11-11 16:14:15 +0300

    Добавил стили для страниц авторизации и  регистрации

--- src/layouts/LoginLayout/LoginLayout.ts
+++ src/layouts/LoginLayout/LoginLayout.ts
@@ -1,17 +1,26 @@
-import { defineComponent, onBeforeMount, ref } from 'vue'
-import { BACKGROUNDS } from '@/config/user'
+import { defineComponent, ref, onBeforeMount } from 'vue'  
+import { BACKGROUNDS } from '@/config/user'  
+import { useCurrentTime } from '@/composables/useCurrentTime'  
 
-export default defineComponent({
-  name: 'LoginLayout',
-  setup() {
-    const backgroundImage = ref<string>('')
-    
-    onBeforeMount(() => {
-      const randomIndex = Math.floor(Math.random() * BACKGROUNDS.length) 
-      backgroundImage.value = BACKGROUNDS[randomIndex] 
-    })
-    return {
-      backgroundImage
-    }
-  },
+export default defineComponent({  
+  name: 'LoginLayout',  
+  setup() {  
+    const backgroundImage = ref<string>('')  
+ 
+    const { weekDay, day, monthName, year, formattedTime } = useCurrentTime()  
+
+    onBeforeMount(() => {  
+      const randomIndex = Math.floor(Math.random() * BACKGROUNDS.length)  
+      backgroundImage.value = BACKGROUNDS[randomIndex]  
+    })  
+
+    return {  
+      backgroundImage,  
+      weekDay,  
+      day,  
+      monthName,  
+      year,  
+      formattedTime,  
+    }  
+  },  
 })
