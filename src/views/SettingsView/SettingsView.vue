6452b5b988dab34ebfb0a19dd1d20ad98efcfa05
Andrei2348 <makarevichandco@gmail.com>
2024-12-09 13:29:12 +0300

    Завершена работа со стилями

--- src/views/SettingsView/SettingsView.vue
+++ src/views/SettingsView/SettingsView.vue
@@ -1,6 +1,6 @@
 <template>  
 	<MainLayout class='settings'>  
-	<h2 class='settings__title'>Настройка профиля</h2>
+		<h2 class='settings__title'>Настройка профиля</h2>
     <ProfileEdit />
 		<PasswordChanger />
 	</MainLayout>  
