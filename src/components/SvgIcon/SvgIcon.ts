4b15d89f81cd766a75a58e7e1edde17631532a6a
Andrei2348 <makarevichandco@gmail.com>
2024-12-10 11:29:12 +0300

    Фиксы картинок

--- src/components/SvgIcon/SvgIcon.ts
+++ src/components/SvgIcon/SvgIcon.ts
@@ -1,27 +1,66 @@
-import { defineComponent, onBeforeMount, ref } from 'vue'
+// import { defineComponent, onBeforeMount, ref } from 'vue'
+
+// export default defineComponent({  
+//   name: 'SvgIcon',
+//    props: {  
+//     icon: {  
+//       type: String,  
+//       required: true  
+//     },
+//     iconClass: {  
+//       type: String,    
+//     }   
+//   },
+  
+//   setup(props) {   
+//     const svgContent = ref<string | null>(null)
+
+//     onBeforeMount(async() => {
+//       const response = await fetch(`../../src/assets/icons/${props.icon}.svg`)
+//       svgContent.value = await response.text()
+//     })
+//     return { 
+//       svgContent,
+//     } 
+//   }  
+// })
+
+
+import { defineComponent, onBeforeMount, ref } from 'vue' 
 
 export default defineComponent({  
-  name: 'SvgIcon',
-   props: {  
+  name: 'SvgIcon',  
+  props: {  
     icon: {  
       type: String,  
       required: true  
     },
     iconClass: {  
-      type: String,    
+      type: String,  
+      default: ''  
     }   
-  },
-  
+  },  
   setup(props) {   
-    const svgContent = ref<string | null>(null)
+    const svgPath = ref('')
+    const isLoading = ref(true)
 
-    onBeforeMount(async() => {
-      const response = await fetch(`../../src/assets/icons/${props.icon}.svg`)
-      svgContent.value = await response.text()
-    })
+    const loadSvg = async () => {
+      isLoading.value = true
+      try {   
+        const { default: path } = await import(`../../assets/icons/${props.icon}.svg`) 
+        svgPath.value = path 
+      } catch (error) {  
+        console.error(`Не удалось загрузить SVG-иконку: ${props.icon}`, error)
+        svgPath.value = ''  
+      } finally {
+        isLoading.value = false
+      }
+    };  
+
+    onBeforeMount(loadSvg) 
     return { 
-      svgContent,
-    } 
+      svgPath,
+      isLoading
+    };  
   }  
-})
-
+});
--- src/components/SvgIcon/SvgIcon.vue
+++ src/components/SvgIcon/SvgIcon.vue
@@ -1,9 +1,12 @@
 <template>  
-    <div 
-      v-html='svgContent'  
-      :class='iconClass'
-    ></div>
-  </template>
-  
-  <script lang="ts" src="./SvgIcon.ts"></script>
+  <div :class="iconClass"> 
+    <img v-if='!isLoading' :src="svgPath" :alt="icon"  />  
+  </div>  
+  <!-- <div 
+    v-html='svgContent'  
+    :class='iconClass'
+  ></div> -->
+</template>
+
+<script lang="ts" src="./SvgIcon.ts"></script>
   
