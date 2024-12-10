74e7bebd76cf5c1066f080102615eed491ab8d28
Andrei2348 <makarevichandco@gmail.com>
2024-12-10 10:53:41 +0300

    Небольшие фиксы

--- src/types/svgIcon.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=svgIcon.js.map
--- src/views/NotFoundView/NotFoundView.js
+++
@@ -1,5 +0,0 @@
-import { defineComponent } from 'vue';
-export default defineComponent({
-    name: 'NotFoundComponent',
-});
-//# sourceMappingURL=NotFoundView.js.map
